1d98e946a569359e55492c4d5ff9698e80fd46dc
Vishwath-Narayana <vishwathnarayanathm19@gmail.com>
2026-04-19 23:00:53 +0530

    added vercel config

--- server/src/server.js
+++ server/src/server.js
@@ -36,7 +36,7 @@ app.use(errorHandler)
 // Init
 initSocket(io)
 connectDB().then(() => {
-  httpServer.listen(process.env.PORT || 5000, () =>
-    console.log(`Server running on port ${process.env.PORT || 5000}`)
+  httpServer.listen(process.env.PORT || 5001, () =>
+    console.log(`Server running on port ${process.env.PORT || 5001}`)
   )
 })
